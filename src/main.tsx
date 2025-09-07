4fd4f97f138fc32ab47b6211797dbbbf255bc7ba
Jagdish Bihari Saxena <jagdishbiharisaxena@gmail.com>
2025-09-07 08:59:30 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import App from './App.tsx';
 import './index.css';
 
 // Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const PUBLISHABLE_KEY = import.meta.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
